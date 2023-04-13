e224866bbce2993c6f1c34408bb98186b3c9dcd4
MohammadWasiq0786 <mohammadwasiq0786.com>
2023-04-13 10:31:22 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Cat-Dog",
+    version="0.0.1",
+    author="wasiq",
+    author_email="gl0427@myamu.ac.in",
+    packages=find_packages(),
+    install_requires=[],
+)
